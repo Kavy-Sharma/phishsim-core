f1aea31529efce375daf5bf672eb21eb2a75dba7
Kavy-Sharma <kavyaboti2005@gmail.com>
2026-08-30 16:13:31 +0530

    Trash Cleanups and Lure Enhancement

--- scratch/scratch_find.py
+++
@@ -1,4 +0,0 @@
-with open('scratch/home_curl_fresh.html', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        if 'selectSandboxScenario(' in line or 'class ToolMiniLoader' in line:
-            print(f'{i+1}: {line.strip()[:100]}')
